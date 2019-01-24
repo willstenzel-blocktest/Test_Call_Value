8cab3d5fce30427c9c4b87ec87509ddee70fd530
willstenzel-blocktest <46688425+willstenzel-blocktest@users.noreply.github.com>
2019-01-24 12:17:26 -0500

    Create Call_Value.sol

---
+++ Call_Value.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.4.25;
+
+contract Call_Value {
+    
+    function doNotRecieve() public payable {
+        throw;
+    }
+    
+    function test_call_value() public {
+        address(this).call.value(msg.value)();
+    }
+}
